f4b99f9fac34fbb037670e170fed4d7ba2ded608
Geraldo Neto <geraldo.lopes@outlook.com>
2023-09-05 18:23:49 +0100

    fix: update toStakeAddress to cover hex addresses

--- src/__tests__/toStakeAddress.spec.ts
+++ src/__tests__/toStakeAddress.spec.ts
@@ -3,8 +3,7 @@ import { toStakeAddress } from "../toStakeAddress";
 describe("toStakeAddress", () => {
   it("keeps the same addr when there is no stake key", () => {
     const result = toStakeAddress(
-      "addr1w8cvzcnclvkua25vx26j72pddatuedsxspm62mr63urs0fctuuzfv",
-      "mainnet"
+      "addr1w8cvzcnclvkua25vx26j72pddatuedsxspm62mr63urs0fctuuzfv"
     );
 
     expect(result).toBe(
@@ -14,33 +13,21 @@ describe("toStakeAddress", () => {
 
   it("converts a mainnet payment address into a stake address", () => {
     const result = toStakeAddress(
-      "addr1qxsgae4x0z3q0hjjqcwya7vzp9f9ex52lrly0wadmfgvhpfyzttqmp0evfcrwxkda77m5lg8c9ta2aafdf83jxyvhw3sgp9v30",
-      "mainnet"
-    );
-
-    expect(result).toBe(
-      "stake1uyjp94sdshukyuphrtx7l0d605ruz474w75k5ncerzxthgc4mghs5"
-    );
-  });
-
-  it("converts a testnet payment address into a stake address", () => {
-    const result = toStakeAddress(
-      "addr_test1qq7rrku33z0d8gcp4xkwa4r0ze2ru2qznhma7twlmv3mj4gdefwpxl0d4a8ld3n56urgxxrc9tqnz6qd52wpdywwnt4qqegjsf",
-      "testnet"
-    );
-
-    expect(result).toBe(
-      "stake_test1uqxu5hqn0hk67nlkce6dwp5rrpuz4sf3dqx698qkj88f46skj0yar"
-    );
-  });
-
-  it("defaults to mainnet if no network is specified", () => {
-    const result = toStakeAddress(
       "addr1qxsgae4x0z3q0hjjqcwya7vzp9f9ex52lrly0wadmfgvhpfyzttqmp0evfcrwxkda77m5lg8c9ta2aafdf83jxyvhw3sgp9v30"
     );
 
     expect(result).toBe(
       "stake1uyjp94sdshukyuphrtx7l0d605ruz474w75k5ncerzxthgc4mghs5"
+    );
+  });
+
+  it("converts a hex address into a stake address", () => {
+    const result = toStakeAddress(
+      "01c59dc72ab0bd904dc9a430bd0b6d9d3b3c2d32dfa3dcf5affd309002fa02bb733c2b7f9684260bc4b53fb8655cb4b31bfd879a127cf684b2"
+    );
+
+    expect(result).toBe(
+      "stake1u8aq9wmn8s4hl95yyc9ufdflhpj4ed9nr07c0xsj0nmgfvs09jmpq"
     );
   });
 });
--- src/toStakeAddress.ts
+++ src/toStakeAddress.ts
@@ -1,22 +1,19 @@
 import { bech32 } from "bech32";
 
 import { hexToBytes } from "./hexToBytes";
+import { toHexAddress } from "./toHexAddress";
 
 const PAYMENT_WITHOUT_STAKING_KEY_LENGTH = 58;
 
-export const toStakeAddress = (paymentAddress: string, network = "mainnet") => {
-  if (paymentAddress.length <= PAYMENT_WITHOUT_STAKING_KEY_LENGTH) {
-    return paymentAddress;
+export const toStakeAddress = (paymentOrHexAddress: string) => {
+  if (paymentOrHexAddress.length <= PAYMENT_WITHOUT_STAKING_KEY_LENGTH) {
+    return paymentOrHexAddress;
   }
 
-  const isMainnet = network === "mainnet";
-  const value = bech32.decode(paymentAddress, 120);
-  const bechWords = bech32.fromWords(value.words);
-  const hexCodes = bechWords.map((w) => w.toString(16).padStart(2, "0"));
-  const hexAddress = hexCodes.join("");
+  const hexAddress = paymentOrHexAddress.startsWith("addr") ? toHexAddress(paymentOrHexAddress) : paymentOrHexAddress;
   const stakeHexAddress = hexAddress.substring(58);
-  const stakeHexBytes = hexToBytes((isMainnet ? "e1" : "e0") + stakeHexAddress);
+  const stakeHexBytes = hexToBytes("e1" + stakeHexAddress);
   const bech32Words = bech32.toWords(stakeHexBytes);
 
-  return bech32.encode(isMainnet ? "stake" : "stake_test", bech32Words, 120);
+  return bech32.encode("stake", bech32Words, 120);
 };
